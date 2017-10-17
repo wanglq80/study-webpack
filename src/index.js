513dd34270bfcf73fc58ebc17a905934302b0f85
wanglq80 <82097676@qq.com>
2017-10-17 17:48:57 +0800

    add style-loader/css-loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 // index.js
 import _ from "lodash";
+import './style.css';
 
 function component () {
 	var element = document.createElement('div');
 
-	// Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-	
-    // Lodash, now improted by this script
+	// Lodash, now improted by this script
     element.innerHTML = _.join(['Hello', 'webpack！'], ' ');
+    element.classList.add('hello');
 
 	return element;
 }
