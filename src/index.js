85bc59c7847620b0c0d794dd1500e0e3e5072237
wanglq80 <82097676@qq.com>
2017-10-18 18:55:39 +0800

    output branch first commit!

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
 // index.js
 import _ from "lodash";
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component () {
 	var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-	// Lodash, now improted by this script
     element.innerHTML = _.join(['Hello', 'webpack！'], ' ');
-    element.classList.add('hello');
+    
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    // 将图像添加到我们现有的 div
-    var myIcon = new Image();
-    myIcon.src = Icon;
-
-    element.appendChild(myIcon);
-
-    console.log(Data);
-
+    element.appendChild(btn);
+    
 	return element;
 }
 
